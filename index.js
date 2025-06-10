e99934bff91c2255353072cf9a57810a8f6b15f5
Andi Surya Priyadi <andi.suryapriyadi@gmail.com>
2025-06-10 21:59:26 +0700

    melepas origin

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const port = 3307;
 const routes = require('./routes/route');
 const db = require('./app/models/index.js'); // ini akan mengakses models/index.js
 
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: '*',  // Mengizinkan semua asal (origin)
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Mengizinkan metode yang umum digunakan
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Header yang diizinkan
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/', routes);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 // Cek koneksi Sequelize ke database
