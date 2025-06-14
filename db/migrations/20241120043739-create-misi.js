68db790909b03ae6be1e1070b82d6af700ba121b
Andi Surya Priyadi <andi.suryapriyadi@gmail.com>
2025-06-14 08:22:46 +0700

    memperbaiki migration misi

--- db/migrations/20241120043739-create-misi.js
+++ db/migrations/20241120043739-create-misi.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Misi', {
+    await queryInterface.createTable('Misis', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Misi');
+    await queryInterface.dropTable('Misis');
   }
 };
