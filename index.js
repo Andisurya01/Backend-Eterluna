3e0ee6bf156179c0c65915ec5447caf3fbe567a5
Andi Surya Priyadi <andi.suryapriyadi@gmail.com>
2025-06-14 07:37:26 +0700

    merubah configurasi cors ke awal

--- index.js
+++ index.js
@@ -7,30 +7,19 @@ const port = Number(process.env.DB_PORT || 55954);
 const routes = require('./routes/route');
 const db = require('./app/models/index.js'); // ini akan mengakses models/index.js
 
-
-app.use((req, res, next) => {
-  console.log('Incoming Origin:', req.headers.origin);
-  console.log('Method:', req.method);
-  const allowedOrigins = [
+app.use(cors({
+  origin: [
     'https://admin-eter-luna.vercel.app',
     'https://eterluna.vercel.app',
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  }
+    'http://localhost:5173',
+    'http://localhost:5174',
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/', routes);
